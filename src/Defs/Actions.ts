05037769218de8a00fcb2913162e6a35325b3124
Philipp Herz <34943569+Pdzly@users.noreply.github.com>
2022-12-16 16:42:37 +0100

    Transfered to ts

---
+++ src/Defs/Actions.ts
@@ -0,0 +1,14 @@
+const Actions:{[key:string]: string} = {
+    NEW: "New event",
+    CON: "Event continued",
+    EXT: "Event extended(time)",
+    EXA: "Event extended(area)",
+    EXB: "Event extended(both time and area)",
+    UPG: "Event upgraded",
+    CAN: "Event cancelled",
+    EXP: "Event expired",
+    COR: "Correction",
+    ROU: "Routine",
+}
+
+export default Actions;
